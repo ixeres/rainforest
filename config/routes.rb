51e55a57a156487f4f81fdb87ed59cc2be77acf1
Devin Leduc <ixeres@gmail.com>
2017-04-20 11:19:17 -0400

    Creating a user model, controller and view.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :products #used to replace individual entries for index, create, new, edit, show, update, destroy. Called 7 RESTful methods
-
+  resources :users, only: [:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
