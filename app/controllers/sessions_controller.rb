314d150db3dc806eaa3b114e95aac53d60d6be22
Devin Leduc <ixeres@gmail.com>
2017-04-20 11:29:48 -0400

    Creating login functionality

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to products_url, notice: "Logged in! ...Jabroni!"
+    else
+      render "new"
+    end
+  end
+  #establishes the login session, checking login credentials against encrypted database. COMPLICATED.
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to products_url, notice: "Logged out! ...Jabroni."
+  end
+  #ends the session by erasing the user id, essentially...?
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :sessions, only: [:new, :create, :destroy] #for login functionality
   resources :products #used to replace individual entries for index, create, new, edit, show, update, destroy. Called 7 RESTful methods
   resources :users, only: [:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
