0a95ef6533d1e9c81e9a7b3863ebcee38c669d25
Devin Leduc <ixeres@gmail.com>
2017-04-20 10:28:35 -0400

    Generating the products controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :products #used to replace individual entries for index, create, new, edit, show, update, destroy. Called 7 RESTful methods
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
